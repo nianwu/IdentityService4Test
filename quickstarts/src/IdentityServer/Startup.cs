aa7779a96ace76754d3477acc42a8f4beddfbba2
zhiyuan yuan <1262163090@qq.com>
2019-04-28 20:59:36 +0800

    完成EF支持

--- quickstarts/src/IdentityServer/Startup.cs
+++ quickstarts/src/IdentityServer/Startup.cs
@@ -5,11 +5,14 @@
 using System;
 using System.Reflection;
 using IdentityServer4;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using System.Linq;
+using IdentityServer4.EntityFramework.DbContexts;
 
 namespace IdentityServer
 {
@@ -51,35 +54,31 @@ namespace IdentityServer
                     };
                 });
 
-            var builder = services.AddIdentityServer()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApis())
-                .AddInMemoryClients(Config.GetClients())
-                .AddTestUsers(Config.GetUsers());
-
-            const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-2.0.0;trusted_connection=yes;";
+            const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=~/IdentityServer4.Quickstart.EntityFramework-2.0.0;trusted_connection=yes;";
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
-            // configure identity server with in-memory stores, keys, clients and scopes
-            services.AddIdentityServer()
-                .AddTestUsers(Config.GetUsers())
-                // this adds the config data from DB (clients, resources)
-                .AddConfigurationStore(options =>
-                {
-                    options.ConfigureDbContext = b =>
-                        b.UseSqlServer(connectionString,
-                            sql => sql.MigrationsAssembly(migrationsAssembly));
-                })
-                // this adds the operational data from DB (codes, tokens, consents)
-                .AddOperationalStore(options =>
-                {
-                    options.ConfigureDbContext = b =>
-                        b.UseSqlServer(connectionString,
-                            sql => sql.MigrationsAssembly(migrationsAssembly));
+            var builder = services.AddIdentityServer()
+                 //.AddInMemoryIdentityResources(Config.GetIdentityResources())
+                 //.AddInMemoryApiResources(Config.GetApis())
+                 //.AddInMemoryClients(Config.GetClients())
+                 .AddTestUsers(Config.GetUsers())
+                 // this adds the config data from DB (clients, resources)
+                 .AddConfigurationStore(options =>
+                 {
+                     options.ConfigureDbContext = b =>
+                         b.UseSqlServer(connectionString,
+                             sql => sql.MigrationsAssembly(migrationsAssembly));
+                 })
+                 // this adds the operational data from DB (codes, tokens, consents)
+                 .AddOperationalStore(options =>
+                 {
+                     options.ConfigureDbContext = b =>
+                         b.UseSqlServer(connectionString,
+                             sql => sql.MigrationsAssembly(migrationsAssembly));
 
-                    // this enables automatic token cleanup. this is optional.
-                    options.EnableTokenCleanup = true;
-                });
+                     // this enables automatic token cleanup. this is optional.
+                     options.EnableTokenCleanup = true;
+                 });
 
             if (Environment.IsDevelopment())
             {
@@ -93,6 +92,8 @@ namespace IdentityServer
 
         public void Configure(IApplicationBuilder app)
         {
+            InitializeDatabase(app);
+
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -105,6 +106,43 @@ namespace IdentityServer
 
             // uncomment, if you wan to add an MVC-based UI
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApis())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
